112619c5e2b4a2a268e0ee59c586a4eb240d7450
KylevdLangemheen <kylevdlangemheen@live.nl>
2021-04-05 19:43:00 +0200

    added some resnet discriminators into the gan

---
+++ Run_files_v2_house/extracthouse.py
@@ -0,0 +1,32 @@
+import os
+import cv2
+import random
+import numpy as np
+
+train_image = []
+test_image = []
+
+current_directory = os.path.dirname(os.path.realpath(__file__))
+house_directory = current_directory + "/RealHouse"
+
+for filename in os.listdir(house_directory):
+    if filename.endswith("frontal.jpg"):
+        img = cv2.imread(house_directory + "/" +filename)
+        print(filename)
+        img_resized = cv2.resize(img,(256,256),interpolation = cv2.INTER_AREA)
+        if random.random() < 0.8:
+            train_image.append(img_resized)
+        else:
+            test_image.append(img_resized)
+
+train_image = np.asarray(train_image)
+test_image = np.asarray(test_image)
+
+print (train_image.shape)
+print (test_image.shape)
+
+with open('train_real.npy', 'wb') as f:
+    np.save(f,train_image,allow_pickle=True)
+with open('test_real.npy', 'wb') as f:
+    np.save(f,test_image,allow_pickle=True)
+
